391f69c1bd4ac3982bc4a2b81dcb87d7f97f4fe7
Aleksander Grzyb <aleksander.grzyb@gmail.com>
2015-04-25 23:21:12 +0300

    add scroll view that shows map image inside

--- TUTCafeterias/View Controllers/SHMapViewController.m
+++ TUTCafeterias/View Controllers/SHMapViewController.m
@@ -8,8 +8,9 @@
 
 #import "SHMapViewController.h"
 
-@interface SHMapViewController ()
-
+@interface SHMapViewController () <UIScrollViewDelegate>
+@property (weak, nonatomic) UIImageView *mapView;
+@property (weak, nonatomic) UIScrollView *scrollView;
 @end
 
 @implementation SHMapViewController
@@ -20,9 +21,57 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self customizeNavigationBar];
+    [self addImage];
     self.view.backgroundColor = [UIColor whiteColor];
 }
 
 
+#pragma mark -
+#pragma mark Image View
+
+- (void)addImage
+{
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    self.scrollView = scrollView;
+    self.scrollView.delegate = self;
+    self.scrollView.minimumZoomScale = 1.0;
+    self.scrollView.maximumZoomScale = 5.0;
+    self.scrollView.zoomScale = 1.0;
+    [self.view addSubview:self.scrollView];
+
+    UIImageView *mapView = [[UIImageView alloc] init];
+    self.mapView = mapView;
+    self.mapView.image = [UIImage imageNamed:@"map"];
+    self.mapView.frame = CGRectMake(0.0f, 0.0f, self.mapView.image.size.width, self.mapView.image.size.height);
+    self.scrollView.contentSize = self.mapView.image.size;
+    [self.scrollView addSubview:self.mapView];
+}
+
+#pragma mark -
+#pragma mark Scroll View Delegate
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return self.mapView;
+}
+
+#pragma mark -
+#pragma mark Navigation Bar Customization
+
+- (void)customizeNavigationBar
+{
+     self.navigationController.navigationBar.tintColor = [UIColor colorWithRed:0.82 green:0.07 blue:0.13 alpha:1];
+    [self.navigationController.navigationBar setTitleTextAttributes:@{
+                                                                     NSFontAttributeName : [UIFont fontWithName:@"Homestead-Regular" size:22.0f],
+                                                                     NSForegroundColorAttributeName : [UIColor colorWithRed:0.82 green:0.07 blue:0.13 alpha:1],
+                                                                     NSBackgroundColorAttributeName : [UIColor clearColor],
+                                                                     NSKernAttributeName : [NSNumber numberWithFloat:1.0f]
+                                                                     }];
+
+
+    [self.navigationItem setTitle:@"Map"];
+
+}
 
 @end
